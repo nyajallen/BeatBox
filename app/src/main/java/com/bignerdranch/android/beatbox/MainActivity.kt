59dfe1a9d7bcd742669e2a0628267d151935e857
nyajallen <allen.nya24@gmail.com>
2020-06-17 19:27:30 -0400

    End of Chapter 20. End of App

--- app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
@@ -30,11 +30,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        beatBox.release()
+    }
+
     private inner class SoundHolder(private val binding: ListItemSoundBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         init {
-            binding.viewModel = SoundViewModel()
+            binding.viewModel = SoundViewModel(beatBox)
         }
 
         fun bind(sound: Sound) {
